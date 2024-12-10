de12ba1d8445356e1f2f11b4d31ce0d130e27393
Heiler Nova <heilernova@outlook.com>
2024-12-09 20:04:39 -0500

    feat(core): crear librería para las utilidades de globales

---
+++ packages/core/src/lib/types.ts
@@ -0,0 +1,9 @@
+export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+export type OmitBy<T, K extends keyof T> = Omit<T, K>;
+
+export type PartialWithout<T, K extends keyof T> = Partial<T> & Pick<T, K>;
+export type OmitWithout<T, K extends keyof T> = Omit<T, keyof T> & Pick<T, K>;
+
+export type ConvertDateToString<T> = {
+    [K in keyof T]: T[K] extends Date ? string : T[K];
+};
