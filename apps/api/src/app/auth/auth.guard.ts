1d12d7fb38ab401c1cf344dbeedcd78833fa2866
Heiler Nova <heilernova@outlook.com>
2024-12-22 11:19:54 -0500

    feat(api): crear el guard para controlar el acceso a la API.

---
+++ apps/api/src/app/auth/auth.guard.ts
@@ -0,0 +1,37 @@
+import { CanActivate, ExecutionContext, HttpException, Injectable } from '@nestjs/common';
+import { Request } from 'express';
+import { isUUID } from 'class-validator';
+import { getService } from '@deploy/api/utils/get-service';
+import { TokensService } from '@deploy/api/models/tokens';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  async canActivate(
+    context: ExecutionContext,
+  ): Promise<boolean> {
+
+    const request: Request = context.switchToHttp().getRequest();
+
+    const token = request.headers["x-app-token"];
+    const tokensService = await getService(TokensService);
+
+    
+    if (typeof token !== "string" || !isUUID(token)){
+      throw new HttpException("Formato del token invalido", 401);
+    }
+
+    const tokenAuth = await tokensService.verify(token);
+
+    if (!tokenAuth){
+      throw new HttpException("Token invalido", 401);
+    }
+
+    if (tokenAuth.exp && tokenAuth.exp < (new Date())){
+      throw new HttpException("El token de acceso ha expirado", 403);
+    }
+
+    request["appTokenA"] = token;
+
+    return true;
+  }
+}
