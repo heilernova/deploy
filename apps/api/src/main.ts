e8db06605ed96a31e7ea648bed25a2061744e81d
Heiler Nova <heilernova@outlook.com>
2024-12-23 08:29:15 -0500

    chore(api): configurar las validaciones de datos.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,7 +3,7 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { initDb } from './app/config/db-init';
@@ -18,6 +18,7 @@ async function bootstrap() {
   }
 
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
