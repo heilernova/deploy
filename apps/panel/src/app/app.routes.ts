3fbc14a6d550b8e47b9bb7b1d4a978be3fa9653c
Heiler Nova <heilernova@outlook.com>
2024-12-23 15:48:43 -0500

    feat(api): crear componente del `layout`

--- apps/panel/src/app/app.routes.ts
+++ apps/panel/src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
-    { path: 'login', loadComponent: () => import('./pages/login-page/login-page.component').then(x => x.LoginPageComponent) }
+    { path: 'login', loadComponent: () => import('./pages/login-page/login-page.component').then(x => x.LoginPageComponent) },
+    { 
+        path: '',
+        loadComponent: () => import('./layout/layout.component').then(x => x.LayoutComponent)
+    }
 ];
