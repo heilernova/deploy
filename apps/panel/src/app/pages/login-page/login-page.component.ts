9d9a7dbff91b38d20ac771c526271b7c029bd099
Heiler Nova <heilernova@outlook.com>
2024-12-23 17:19:47 -0500

    feat(panel): agregar funcionalidad a formulario del login.

--- apps/panel/src/app/pages/login-page/login-page.component.ts
+++ apps/panel/src/app/pages/login-page/login-page.component.ts
@@ -1,11 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from "ng-zorro-antd/button";
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { AuthService } from '@deploy/panel/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    NzFormModule,
+    NzInputModule,
+    NzButtonModule
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
+  private readonly _auth = inject(AuthService);
+  private readonly _message = inject(NzMessageService);
+  private readonly _router = inject(Router);
 
+  protected readonly credentials = new FormGroup({
+    username: new FormControl<string>("", { nonNullable: true, validators: Validators.required }),
+    password: new FormControl<string>("", { nonNullable: true, validators: Validators.required }),
+  });
+
+  protected readonly disableButtonSignIn = signal<boolean>(true);
+
+  constructor(){
+    this.credentials.statusChanges.subscribe(status => this.disableButtonSignIn.set(status !== "VALID"));
+  }
+
+  protected onClickSignIn(): void {
+    if (this.credentials.invalid){
+      this._message.warning("Falta campos por completar.");
+      return;
+    }
+
+    const credentials = this.credentials.getRawValue();
+    this.credentials.disable();
+    this._auth.signIn(credentials)
+    .then(() => {
+      this._message.success("Bienvenido.");
+      this._router.navigate(["/"]);
+    })
+    .catch(err => {
+      this._message.warning(err.error.message);
+      this.credentials.enable();
+    })
+
+  }
 }
