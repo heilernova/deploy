68cc645a38f9d5709a103e6bb312f29119652e19
Heiler Nova <heilernova@outlook.com>
2024-12-24 16:06:24 -0500

    feat(panel): agregar el token de acceso a los headers.

--- apps/panel/src/app/config/api.interceptor.ts
+++ apps/panel/src/app/config/api.interceptor.ts
@@ -1,7 +1,17 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
 import { environment } from '../../environments/environment';
 
 export const apiInterceptor: HttpInterceptorFn = (req, next) => {
+  const auth = inject(AuthService);
   const url = `${environment.API_URL_BASE}/${req.url}`;
-  return next(req.clone({ url }));
+  let headers = req.headers;
+
+  const session = auth.getSession();
+  if (session){
+    headers = headers.append("X-App-Token", session.token);
+  }
+
+  return next(req.clone({ url, headers }));
 };
