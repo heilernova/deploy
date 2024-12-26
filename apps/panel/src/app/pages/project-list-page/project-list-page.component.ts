c8cf3532abea2d9443c0a207204bd28b82c12d5e
Heiler Nova <heilernova@outlook.com>
2024-12-25 21:49:40 -0500

    feat(panel): cargar la información de los proyectos

--- apps/panel/src/app/pages/project-list-page/project-list-page.component.ts
+++ apps/panel/src/app/pages/project-list-page/project-list-page.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { Project } from '@deploy/panel/data/projects/project';
+import { ProjectsDataSourceService } from '@deploy/panel/data/projects/projects-data-source.service';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzInputModule } from 'ng-zorro-antd/input';
 
 @Component({
   selector: 'app-project-list-page',
-  imports: [],
+  imports: [
+    NzInputModule,
+    NzButtonModule
+  ],
   templateUrl: './project-list-page.component.html',
   styleUrl: './project-list-page.component.scss'
 })
-export class ProjectListPageComponent {
-
+export class ProjectListPageComponent implements OnInit {
+  private readonly _projectsDataSource = inject(ProjectsDataSourceService);
+  protected readonly list = signal<Project[]>([]);
+  
+  ngOnInit(): void {
+    this._projectsDataSource.getAll().then(list => {
+      this.list.set(list);
+    })
+  }
 }
