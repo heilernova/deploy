4d5c6a80f02f44bccf3841bfb12e80c21b127aa3
Heiler Nova <heilernova@outlook.com>
2024-12-25 21:48:12 -0500

    fix(core): corregir ConvertDateToString

--- packages/core/src/lib/types.ts
+++ packages/core/src/lib/types.ts
@@ -5,5 +5,5 @@ export type PartialWithout<T, K extends keyof T> = Partial<T> & Pick<T, K>;
 export type OmitWithout<T, K extends keyof T> = Omit<T, keyof T> & Pick<T, K>;
 
 export type ConvertDateToString<T> = {
-    [K in keyof T]: T[K] extends Date ? string : T[K];
-};
+    [K in keyof T]: T[K] extends Date ? string : T[K] extends Date | null ? string | null : T[K] 
+}
