aa4f4a9f116dc7616348063df46851d7307c6d25
Heiler Nova <heilernova@outlook.com>
2025-01-16 12:18:45 -0500

    feat(panel): validamos el error 401 enl a peticiones http realizadas al servidor

--- apps/panel/src/app/config/api.interceptor.ts
+++ apps/panel/src/app/config/api.interceptor.ts
@@ -1,10 +1,15 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { environment } from '../../environments/environment';
+import { catchError, throwError } from 'rxjs';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { Router } from '@angular/router';
 
 export const apiInterceptor: HttpInterceptorFn = (req, next) => {
   const auth = inject(AuthService);
+  const nzMessage = inject(NzMessageService);
+  const router = inject(Router);
   const url = `${environment.API_URL_BASE}/${req.url}`;
   let headers = req.headers;
 
@@ -13,5 +18,16 @@ export const apiInterceptor: HttpInterceptorFn = (req, next) => {
     headers = headers.append("X-App-Token", session.token);
   }
 
-  return next(req.clone({ url, headers }));
+  return next(req.clone({ url, headers })).pipe(
+    catchError(err => {
+      if (err instanceof HttpErrorResponse){
+        if (err.status == 401){
+          nzMessage.error("Tú sesión ha espirado.");
+          auth.logout();
+          router.navigate(["/login"]);
+        }
+      }
+      return throwError(() => err);
+    })
+  );
 };
