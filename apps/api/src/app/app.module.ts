0efff0497a9be6717c4c4680a6166535afdf78a8
Heiler Nova <heilernova@outlook.com>
2025-01-18 11:02:38 -0500

    feat(api): add file replacements for production build and serve static files from panel

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,10 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommonModule } from './common/common.module';
 import { ModelsModule } from './models/models.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { ControllersModule } from './controllers/controllers.module';
+import { join } from 'node:path';
 
 @Module({
-  imports: [CommonModule, ModelsModule, ControllersModule],
+  imports: [
+    CommonModule,
+    ModelsModule,
+    ControllersModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../panel/browser'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
